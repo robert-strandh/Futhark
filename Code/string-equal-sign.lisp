318ac3bc78dc212720a45b66df5dc2401886bbb7
Robert Strandh <robert.strandh@gmail.com>
2024-12-23 16:30:12 +0100

    New component containing definition of STRING=.

---
+++ Code/string-equal-sign.lisp
@@ -0,0 +1,11 @@
+(cl:in-package #:futhark)
+
+(defun string= (string1 string2 &key (start1 0) end1 (start2 0) end2)
+  (with-canonicalized-and-checked-strings
+      ((string1 string1) (start1 start1) (end1 end1)
+       (string2 string2) (start2 start2) (end2 end2))
+    (let ((pos (mismatch string1 string2
+                         :test #'char=
+                         :start1 start1 :end1 end1
+                         :start2 start2 :end2 end2)))
+      (null pos))))
