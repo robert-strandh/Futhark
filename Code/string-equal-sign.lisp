1c4bc2ce16d425063c5b4801e01b3da25b6eb032
Robert Strandh <robert.strandh@gmail.com>
2024-12-23 17:06:52 +0100

    Add INLINE/NOTILINE proclamations.

--- Code/string-equal-sign.lisp
+++ Code/string-equal-sign.lisp
@@ -1,5 +1,7 @@
 (cl:in-package #:futhark)
 
+(declaim (inline string=))
+
 (defun string= (string1 string2 &key (start1 0) end1 (start2 0) end2)
   (with-canonicalized-and-checked-strings
       ((string1 string1) (start1 start1) (end1 end1)
@@ -9,3 +11,5 @@
                          :start1 start1 :end1 end1
                          :start2 start2 :end2 end2)))
       (null pos))))
+
+(declaim (notinline string=))
