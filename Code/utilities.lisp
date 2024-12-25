a3d3469a7b50953fb819a85c886724fcc2ca9c5f
Robert Strandh <robert.strandh@gmail.com>
2024-12-25 13:35:00 +0100

    Define function ENSURE-STRING-TYPE.

--- Code/utilities.lisp
+++ Code/utilities.lisp
@@ -107,6 +107,9 @@
      (check-bounding-indices ,string1-var ,start1-var ,end1-var)
      ,@body))
 
+(defun ensure-string-type (string-designator)
+  (check-type string-designator cl:string))
+
 (defmacro ensure-fresh-string ((string-var) &body body)
   `(let ((,string-var
            (if (stringp ,string-var) (copy-seq ,string-var) ,string-var)))
