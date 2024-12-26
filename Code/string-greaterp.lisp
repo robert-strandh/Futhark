64ede9989ae75ce492997d753378644540a94f8f
Robert Strandh <robert.strandh@gmail.com>
2024-12-26 11:46:39 +0100

    Use the second return value of the comparison function.

--- Code/string-greaterp.lisp
+++ Code/string-greaterp.lisp
@@ -5,7 +5,8 @@
 (defun string-greaterp (string1 string2 &key (start1 0) end1 (start2 0) end2)
   (with-canonicalized-and-checked-strings
       ((string1 start1 end1) (string2 start2 end2))
-    (ecase (compare-equal string1 start1 end1 string2 start2 end2)
-      (> t) ((< =) nil))))
+    (multiple-value-bind (result index)
+        (compare-equal string1 start1 end1 string2 start2 end2)
+    (ecase result (> index) ((< =) nil)))))
 
 (declaim (notinline string-greaterp))
