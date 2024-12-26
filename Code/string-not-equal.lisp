944d896c2dc2f74f2345d8824d19d248bbebe052
Robert Strandh <robert.strandh@gmail.com>
2024-12-26 15:14:39 +0100

    Split STRING-NOT-EQUAL into ordinary funciton and -CORE function.

--- Code/string-not-equal.lisp
+++ Code/string-not-equal.lisp
@@ -1,11 +1,18 @@
 (cl:in-package #:futhark)
 
-(declaim (inline string-not-equal))
+(declaim (inline string-not-equal-core))
 
-(defun string-not-equal (string1 string2 &key (start1 0) end1 (start2 0) end2)
+(defun string-not-equal-core (string1 string2 start1 end1 start2 end2)
   (with-canonicalized-and-checked-strings
       ((string1 start1 end1) (string2 start2 end2))
     (ecase (compare-equal string1 start1 end1 string2 start2 end2)
       (= nil) ((< >) t))))
+
+(declaim (notinline string-not-equal-core))
+
+(declaim (inline string-not-equal))
+
+(defun string-not-equal (string1 string2 &key (start1 0) end1 (start2 0) end2)
+  (string-not-equal-core string1 string2 start1 end1 start2 end2))
 
 (declaim (notinline string-not-equal))
