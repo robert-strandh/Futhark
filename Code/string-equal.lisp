effe3b636fc44ded721e68afdb430049670dcb11
Robert Strandh <robert.strandh@gmail.com>
2024-12-26 14:26:23 +0100

    Split STRING-EQUAL into ordinary funciton and -CORE function.

--- Code/string-equal.lisp
+++ Code/string-equal.lisp
@@ -1,11 +1,18 @@
 (cl:in-package #:futhark)
 
-(declaim (inline string-equal))
+(declaim (inline string-equal-core))
 
-(defun string-equal (string1 string2 &key (start1 0) end1 (start2 0) end2)
+(defun string-equal-core (string1 string2 start1 end1 start2 end2)
   (with-canonicalized-and-checked-strings
       ((string1 start1 end1) (string2 start2 end2))
     (ecase (compare-equal string1 start1 end1 string2 start2 end2)
       (= t) ((< >) nil))))
+
+(declaim (notinline string-equal-core))
+
+(declaim (inline string-equal))
+
+(defun string-equal (string1 string2 &key (start1 0) end1 (start2 0) end2)
+  (string-equal-core string1 string2 start1 end1 start2 end2))
 
 (declaim (notinline string-equal))
