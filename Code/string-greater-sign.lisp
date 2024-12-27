0221292dffc12fd3c5f6e2f438a59b38e32a023a
Robert Strandh <robert.strandh@gmail.com>
2024-12-27 05:16:45 +0100

    Split STRING> into ordinary funciton and -CORE function.

--- Code/string-greater-sign.lisp
+++ Code/string-greater-sign.lisp
@@ -1,12 +1,19 @@
 (cl:in-package #:futhark)
 
-(declaim (inline string>))
+(declaim (inline string>-core))
 
-(defun string> (string1 string2 &key (start1 0) end1 (start2 0) end2)
+(defun string>-core (string1 string2 start1 end1 start2 end2)
   (with-canonicalized-and-checked-strings
       ((string1 start1 end1) (string2 start2 end2))
     (multiple-value-bind (result index)
         (compare= string1 start1 end1 string2 start2 end2)
     (ecase result (> index) ((< =) nil)))))
+
+(declaim (notinline string>-core))
+
+(declaim (inline string>))
+
+(defun string> (string1 string2 &key (start1 0) end1 (start2 0) end2)
+  (string>-core string1 string2 start1 end1 start2 end2))
 
 (declaim (notinline string>))
