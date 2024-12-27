6d51b9ec38cd8bd4fb09abc295c205c901419046
Robert Strandh <robert.strandh@gmail.com>
2024-12-27 05:43:34 +0100

    Split STRING-GREATERP into ordinary funciton and -CORE function.

--- Code/string-greaterp.lisp
+++ Code/string-greaterp.lisp
@@ -1,12 +1,19 @@
 (cl:in-package #:futhark)
 
-(declaim (inline string-greaterp))
+(declaim (inline string-greaterp-core))
 
-(defun string-greaterp (string1 string2 &key (start1 0) end1 (start2 0) end2)
+(defun string-greaterp-core (string1 string2 start1 end1 start2 end2)
   (with-canonicalized-and-checked-strings
       ((string1 start1 end1) (string2 start2 end2))
     (multiple-value-bind (result index)
         (compare-equal string1 start1 end1 string2 start2 end2)
     (ecase result (> index) ((< =) nil)))))
+
+(declaim (notinline string-greaterp-core))
+
+(declaim (inline string-greaterp))
+
+(defun string-greaterp (string1 string2 &key (start1 0) end1 (start2 0) end2)
+  (string-greaterp-core string1 string2 start1 end1 start2 end2))
 
 (declaim (notinline string-greaterp))
