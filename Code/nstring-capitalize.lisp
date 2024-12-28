8a933789dd928e1ab31c6b3732a3d28959bbc34e
Robert Strandh <robert.strandh@gmail.com>
2024-12-28 17:34:57 +0100

    Define compiler macro for NSTRING-CAPITALIZE.

--- Code/nstring-capitalize.lisp
+++ Code/nstring-capitalize.lisp
@@ -1,8 +1,8 @@
 (cl:in-package #:futhark)
 
-(declaim (inline nstring-capitalize))
+(declaim (inline nstring-capitalize-core))
 
-(defun nstring-capitalize (string &key (start 0) (end nil))
+(defun nstring-capitalize-core (string start end)
   (ensure-string-type string)
   (let ((state nil))
     (with-canonicalized-and-checked-string ((string start end))
@@ -16,4 +16,14 @@
               (setf state t))))))
   string)
 
+(declaim (inline nstring-capitalize-core))
+
+(defun nstring-capitalize (string &key (start 0) (end nil))
+  (nstring-capitalize-core string start end))
+
 (declaim (notinline nstring-capitalize))
+
+(define-compiler-macro nstring-capitalize (&whole form &rest arguments)
+  (if (one-string-compiler-macro-possible-p arguments)
+      (compute-one-string-compiler-macro arguments 'nstring-capitalize)
+      form))
