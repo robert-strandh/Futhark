3f900b0cf5c8a9e6223378bc3cd5ecfb55c9b683
Robert Strandh <robert.strandh@gmail.com>
2024-12-28 15:45:53 +0100

    Define compiler macro for STRING-UPCASE.

--- Code/string-upcase.lisp
+++ Code/string-upcase.lisp
@@ -1,12 +1,24 @@
 (cl:in-package #:futhark)
 
-(declaim (inline string-upcase))
+(declaim (inline string-upcase-core))
 
-(defun string-upcase (string &key (start 0) end)
+(defun string-upcase-core (string start end)
   (ensure-fresh-string (string)
     (with-canonicalized-and-checked-string ((string start end))
       (for-each-relevant-character (character string start end)
         (setf character (char-upcase character))))
     string))
 
+(declaim (notinline string-upcase-core))
+
+(declaim (inline string-upcase))
+
+(defun string-upcase (string &key (start 0) end)
+  (string-upcase-core string start end))
+
 (declaim (notinline string-upcase))
+
+(define-compiler-macro string-upcase (&whole form &rest arguments)
+  (if (one-string-compiler-macro-possible-p arguments)
+      (compute-one-string-compiler-macro arguments 'string-upcase)
+      form))
