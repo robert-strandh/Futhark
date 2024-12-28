27708c07157de60b977ec7ef863d25b0921285ef
Robert Strandh <robert.strandh@gmail.com>
2024-12-28 15:25:09 +0100

    Define compiler macro for STRING-NOT-LESSP.

--- Code/string-not-lessp.lisp
+++ Code/string-not-lessp.lisp
@@ -1,12 +1,24 @@
 (cl:in-package #:futhark)
 
-(declaim (inline string-not-lessp))
+(declaim (inline string-not-lessp-core))
 
-(defun string-not-lessp (string1 string2 &key (start1 0) end1 (start2 0) end2)
+(defun string-not-lessp-core (string1 string2 start1 end1 start2 end2)
   (with-canonicalized-and-checked-strings
       ((string1 start1 end1) (string2 start2 end2))
     (multiple-value-bind (result index)
         (compare-equal string1 start1 end1 string2 start2 end2)
       (ecase result ((> =) index) (< nil)))))
 
+(declaim (notinline string-not-lessp-core))
+
+(declaim (inline string-not-lessp))
+
+(defun string-not-lessp (string1 string2 &key (start1 0) end1 (start2 0) end2)
+  (string-not-lessp-core string1 string2 start1 end1 start2 end2))
+
 (declaim (notinline string-not-lessp))
+
+(define-compiler-macro string-not-lessp (&whole form &rest arguments)
+  (if (two-string-compiler-macro-possible-p arguments)
+      (compute-two-string-compiler-macro arguments 'string-not-lessp-core)
+      form))
