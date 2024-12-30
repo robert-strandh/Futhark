ebbadc8add56b337683dd5f60dc5946691f178f1
Robert Strandh <robert.strandh@gmail.com>
2024-12-30 07:46:08 +0100

    Provide documentation strings for STRING/=.

--- Code/string-not-equal-sign.lisp
+++ Code/string-not-equal-sign.lisp
@@ -21,3 +21,22 @@
   (if (two-string-compiler-macro-possible-p arguments)
       (compute-two-string-compiler-macro arguments 'string/=-core)
       form))
+
+(setf (documentation 'string/= 'function)
+      (format nil
+              "Syntax: string/= string1 string2 &key start1 end1 start2 end2~@
+               ~@
+               ~a CHAR=.~@
+               ~@
+               ~a~@
+               ~@
+               ~a~@
+               ~@
+               ~a~@
+               ~@
+               ~a"
+              *if-strings-are-not-equal*
+              *string1-and-string2-are-string-designators*
+              *this-function-calls-the-string-function*
+              *start1/2-and-end1/2-are-bounding-index-designators*
+              *definition-of-bounding-index-designators*))
