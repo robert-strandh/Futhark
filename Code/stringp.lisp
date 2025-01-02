ca3e3b6435d5e8e39f3d883c750ca521d3d35738
Robert Strandh <robert.strandh@gmail.com>
2025-01-02 14:27:23 +0100

    Provide documentation string for STRINGP.

--- Code/stringp.lisp
+++ Code/stringp.lisp
@@ -9,3 +9,10 @@
 (defmethod stringp ((object cl:string))
   (declare (ignorable object))
   t)
+
+(setf (documentation 'stringp 'function)
+      (format nil
+              "Syntax: stringp object~@
+               ~@
+               If OBJECT is a string, then return T.  Otherwise,~@
+               return NIL."))
