9dadb5a4356f8bd7657d544ff6e51b069e6ab63f
Robert Strandh <robert.strandh@gmail.com>
2025-01-03 14:52:50 +0100

    Provide documentation string for MAKE-STRING.

--- Code/make-string.lisp
+++ Code/make-string.lisp
@@ -7,3 +7,13 @@
   (make-array size
               :initial-element initial-element
               :element-type element-type))
+
+(setf (documentation 'make-string 'function)
+      (format nil
+              "Syntax: make-string size &key initial-element element-type~@
+               ~@
+               Return a simple string with SIZE instances of INITIAL-ELEMENT~@
+               in it.  The default value of INITIAL-ELEMENT is #\Space.~@
+               The default value of ELEMENT-TYPE is CHARACTER.  This function~@
+               calls MAKE-ARRAY with SIZE and the keyword arguments~@
+               INITIAL-ELEMENT and ELEMENT-TYPE."))
