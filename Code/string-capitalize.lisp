1c8071d948e0a46b841c96f5d90336991d4cd074
LdBeth <andpuke@foxmail.com>
2025-01-06 14:55:56 -0600

    Provide documentation string for STRING-CAPITALIZE and NSTRING-CAPITALIZE

--- Code/string-capitalize.lisp
+++ Code/string-capitalize.lisp
@@ -29,3 +29,25 @@
   (if (one-string-compiler-macro-possible-p arguments)
       (compute-one-string-compiler-macro arguments 'string-capitalize-core)
       form))
+
+(setf (documentation 'string-capitalize 'function)
+      (format nil
+              "Syntax: string-capitalize string &key start end~@
+               ~@
+               Return a string which is like STRING, except that~@
+               in the interval of STRING designated by START and~@
+               END, the first character of each word is converted~@
+               to uppercase, by a call to CHAR-UPCASE, and any other~@
+               characters are converted to lowercase by CHAR-DOWNCASE.~@
+               ~@
+               ~a~@
+               ~@
+               ~a~@
+               ~@
+               ~a~@
+               ~@
+               ~a"
+              *string-is-a-string-designator*
+              *this-function-calls-the-string-function*
+              *start-and-end-are-bounding-index-designators*
+              *definition-of-bounding-index-designators*))
